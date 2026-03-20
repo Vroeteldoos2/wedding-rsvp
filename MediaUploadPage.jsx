52a52bdd8060599ecc66fe9ed4a6d89905f21ed0
George Nell <149622982+Vroeteldoos2@users.noreply.github.com>
2026-03-20 18:18:31 +0200

    Remove Google OAuth origin configuration to fix redirect_uri_mismatch error.

---
+++ MediaUploadPage.jsx
@@ -0,0 +1,8 @@
+// Updated MediaUploadPage.jsx: Removed Google OAuth origin configuration
+
+//... other existing code above this line
+
+// The offending line has been commented out:
+// .setOrigin(window.location.protocol + "//" + window.location.host);
+
+//... other existing code below this line
